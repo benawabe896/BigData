dcd7c3d3e6d4720bfe630b4e06e3baf0d851da34
Sony <sony@Sonys-Mac-mini.local>
2013-05-09 08:18:54 -0700

    Adjustment again

--- 3_Problem/term_sentiment.py
+++ 3_Problem/term_sentiment.py
@@ -1,29 +1,38 @@
 import sys, json
 
-def main():
-	sent_file = open(sys.argv[1])
-	tweet_file = open(sys.argv[2])
-
+def getScores(sent_file):
 	scores = {} # initialize an empty dictionary
 	for line in sent_file:
 		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
 		scores[term] = int(score)  # Convert the score to an integer.
+	return scores
 
+def main():
+	sent_file = open(sys.argv[1])
+	tweet_file = open(sys.argv[2])
+
+	scores = getScores(sent_file)
+
+	newscores = {}
 	for line in tweet_file:
-		sent_total = 0
+		tweet_total = 0
 		line = json.loads(line)
 		if "text" in line:
 			notFound = []
 			for word in line['text'].encode('utf-8').split():
-				if word in scores:
-					sent_total += scores[word]
-				else:
+				if word not in scores and word not in newscores:
+					newscores[word] = 0
 					notFound.append(word)
+				else:
+					if word in scores:
+						tweet_total += scores[word]
 			for word in notFound:
-				scores[word] = sent_total
+				newscores[word] += tweet_total
 
-	for word in scores:
-		print word, " ", scores[word]
+	for word in newscores:
+		print word, " ", newscores[word]
 
 if __name__ == '__main__':
 	main()
+
+
